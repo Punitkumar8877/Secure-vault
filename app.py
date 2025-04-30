a8cc2171f7ca02761afa826937defeb1c5aef827
Punitkumar8877 <punitkumar62020@gmail.com>
2025-04-30 16:12:42 +0530

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True 
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'punitkproject78@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kwzs trus ikem xugs'
+app.config['MAIL_PASSWORD'] = '#####################'
 app.config['MAIL_DEFAULT_SENDER'] = 'punitkproject78@gmail.com'
 
 mail = Mail(app)
